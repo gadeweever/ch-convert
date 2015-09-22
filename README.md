5162f58e50cf8c02c453bf7c9e86084a575ad16e
gadeweever <gamaldeweever@gmail.com>
2015-09-22 16:59:00 -0400

    v1

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # ch-convert
-ch-convert is a library for converting Simplified Chinese characters to Traditional and vice versa
+ch-convert is a library for converting Simplified Chinese characters to Traditional and vice versa. This module is simply a serverside transformation of sionguis clientside library.
 
 # usage
 
@@ -13,8 +13,12 @@ Then to use the converter specify the flag you would like to use :
 
 ```
 //converts simplified to traditional
-var converted_string = converter.convert("some simplified character", converter.flagTrad);
+var converted_string = converter.convert("some simplified character", converter.Trad);
 
 //converts traditional to simplified
-var converted_string = converter.convert("some traditional character", converter.flagSimp);
+var converted_string = converter.convert("some traditional character", converter.Simp);
 ```
+
+# credits
+
+Special thanks http://siongui.github.io/ for the conversion tables
---
+++ converter.js
@@ -0,0 +1,70 @@
+﻿var simptable = require('./tables/s2t.js');
+var tradtable = require('./tables/t2s.js');
+var Simp = "simplified";
+var Trad = "traditional";
+
+var zhEncodesSimp = ["gb2312", "gbk", "x-gbk", "gb18030", "hz-gb-2312", "iso-2022-cn"];
+var zhEncodesTrad = ["big5", "big5-hkscs", "x-euc-tw"];
+var zhEncodesAll = zhEncodesSimp.concat(zhEncodesTrad, ["utf-7", "utf-8", "utf-16le", "x-user-defined"]);
+
+var t2s = tradtable.getTable();
+var s2t = simptable.getTable();
+
+var maxSTLen = 1;
+var maxTSLen = 1;
+
+var curZhFlag = "";
+
+module.exports = {
+
+        convert: function (str, zhflag) {
+            var leng = 4;
+            var zmap = null;
+
+            if (zhflag == Simp)
+            {
+                /
+                zmap = t2s;
+                leng = Math.min(maxTSLen, str.length);
+            } else
+            {
+
+                zmap = s2t;
+                leng = Math.min(maxSTLen, str.length);
+            }
+
+
+            str = str.split("");
+            for (var i = 0, c = str.length; i < c; i++)
+            {
+                str[i] = zmap[str[i]] || str[i];
+            }
+            str = str.join("");
+
+
+            var txt = "", s = "", bol = true;
+            for (var i = 0, c = str.length; i < c;)
+            {
+                bol = true;
+                for (var j = leng; j > 1; j--)
+                {
+                    s = str.substr(i, j);
+                    if (s in zmap)
+                    {
+                        txt += zmap[s];
+                        i += j;
+                        bol = false;
+                        break;
+                    }
+                }
+
+                if (bol)
+                {
+                    txt += str.substr(i, 1);
+                    i++;
+                }
+            }
+            if (txt != "") str = txt;
+            return str;
+        }
+};
